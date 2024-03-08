7b32a5c1f61aad5275fcf18cf85bf5c4e01887eb
{Krishna <gkrishnasree5@gmail.com>
2024-03-08 18:52:31 +0530

    coding done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,84 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+const anilist = [
+  {
+    id: 1,
+    img: "https://cdn.myanimelist.net/images/anime/1517/92767l.jpg",
+    title: "Tokyo Ghoul",
+  },
+  {
+    id: 2,
+    img: "https://cdn.myanimelist.net/images/anime/10/47347.jpg",
+    title: "Shingeki no Kyojin",
+  },
+  {
+    id: 3,
+    img: "https://cdn.myanimelist.net/images/anime/1129/135442.jpg",
+    title: "Cyberpunk: Edgerunners",
+  },
+  {
+    id: 4,
+    img: "https://cdn.myanimelist.net/images/anime/10/77957.jpg",
+    title: "Erased",
+  },
+  {
+    id: 5,
+    img: "https://cdn.myanimelist.net/images/anime/4/75509.jpg",
+    title: "Another",
+  },
+  {
+    id: 6,
+    img: "https://cdn.myanimelist.net/images/anime/1171/109222.jpg",
+    title: "Jujutsu Kaisen",
+  },
+  {
+    id: 7,
+    img: "https://cdn.myanimelist.net/images/anime/1458/113542.jpg",
+    title: "Link Click",
+  },
+  {
+    id: 8,
+    img: "https://cdn.myanimelist.net/images/anime/9/9453.jpg",
+    title: "Death Note",
+  },
+  {
+    id: 9,
+    img: "https://cdn.myanimelist.net/images/anime/3/10243.jpg",
+    title: "Serial Experiments Lain",
+  },
+  {
+    id: 10,
+    img: "https://cdn.myanimelist.net/images/anime/5/29347.jpg",
+    title: "Ergo Proxy",
+  },
+];
+
+const App = () => {
+  return (
+    <div id="container">
+      {anilist.map((a) => {
+        return <Card img={a.img} title={a.title} key={a.id} />;
+      })}
+      <p>And many more...</p>
+    </div>
+  );
+};
+
+const Card = ({ img, title }) => {
+  return (
+    <div id="item">
+      <img src={img} />
+      <p>{title}</p>
+    </div>
+  );
+};
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
